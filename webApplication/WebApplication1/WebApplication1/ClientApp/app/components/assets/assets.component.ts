fa39b617efd63696362026b3ce012e00a875573b
Xing Weng <xweng49@gmail.com>
2018-02-16 06:22:40 +0000

    added missing files

---
+++ webApplication/WebApplication1/WebApplication1/ClientApp/app/components/assets/assets.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'assets',
+    templateUrl: './assets.component.html'
+})
+export class AssetsComponent {
+    public assets: Assets[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/SampleData/Assets').subscribe(result => {
+            this.assets = result.json() as Assets[];
+        }, error => console.error(error));
+    }
+}
+
+interface Assets {
+    classId: string;
+    assetId: string;
+    owner: string;
+    value: string;
+}
--- webApplication/WebApplication1/WebApplication1/ClientApp/app/components/webapp/webapp.component.ts
+++
@@ -1,8 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-    selector: 'webapp',
-    templateUrl: './webapp.component.html'
-})
-export class webappComponent {
-}
