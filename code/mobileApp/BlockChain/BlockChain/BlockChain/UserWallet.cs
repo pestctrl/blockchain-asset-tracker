09afdc1aa376b86c2a6a9a347d1543d6b82c2d55
Benson Chu <bensonchu457@gmail.com>
2018-02-20 17:21:18 +0000

    Added mobile app

---
+++ code/mobileApp/BlockChain/BlockChain/BlockChain/UserWallet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChain
+{
+    public class UserWallet
+    {
+        public List<String> assets = new List<String>()
+        {
+            "Asset A",
+            "Asset B",
+            "Asset C"
+        };
+
+        public String SelectedAsset(String _asset)
+        {
+            String result = "";
+            foreach (String asset in assets)
+            {
+                if (asset == _asset)
+                {
+                    result = _asset;
+                }
+            }
+            return result;
+        }
+
+        public String GetReceiverAddress(String _receiverAddress)
+        {
+            
+            return _receiverAddress;
+        }
+
+        public String Send(String _receiverAddress, String _asset)
+        {
+            return "Send " + _asset + " to " + _receiverAddress;
+        }
+
+        public void ReceivedAsset(String _asset)
+        {
+            assets.Add(_asset);
+        }
+    }
+}
