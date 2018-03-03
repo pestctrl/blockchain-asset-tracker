7b57ea6177082f9214ef7c900979a9604984e6ac
Benson Chu <bensonchu457@gmail.com>
2018-03-03 03:19:35 +0000

    Added test project

---
+++ code/API/consoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpCleint();
+            Console.WriteLine("Hello World!");
+            
+            // Making a GET request is the easiest.
+            // Just call the GetStringAsync method with the correct URL
+            var propertiesJson = await client.GetStringAsync("129.213.108.211:3000/api/org.acme.biznet.Property");
+            Console.WriteLine(propertiesJson);
+        }
+    }
+}
