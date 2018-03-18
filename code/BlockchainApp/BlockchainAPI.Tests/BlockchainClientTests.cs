f91e34a703c84374b32a352238e204e0e8b14060
Benson Chu <bensonchu457@gmail.com>
2018-03-18 01:31:38 +0000

    Added tests

---
+++ code/BlockchainApp/BlockchainAPI.Tests/BlockchainClientTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlockchainAPI.Tests
+{
+    [TestClass]
+    public class BlockchainClientTests
+    {
+        [TestMethod]
+        public void CanaryTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ConstructorOfBlockchainMethodShouldSetUsername()
+        {
+            BlockchainClient client = new BlockchainClient("TRADER1");
+
+            Assert.IsTrue(client.username == "TRADER1");
+        }
+    }
+}
---
+++ code/BlockchainApp/BlockchainAPI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlockchainAPI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlockchainAPI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12fbd34f-f9dc-4b2f-bd3c-da16f74586a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
