c608619a499586ae430729bf4c1cf67d14c8a0ca
Tom Vuong <Ntvuong@uh.edu>
2018-03-18 04:57:41 +0000

    registerPage Upload

---
+++ code/BlockchainApp/BlockchainApp/BlockchainApp/RegisterPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BlockchainAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlockchainApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RegisterPage : ContentPage
+	{
+		public RegisterPage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void CompletedRegister(object sender, EventArgs  e)
+        {
+            BlockchainClient blockChainClient = new BlockchainClient(userId.Text);
+
+            if (blockChainClient.userExist)
+            {
+                await DisplayAlert("Alert", "The user Id have been taken", "Ok");
+            }
+            else
+            {
+                blockChainClient.RegisterNewTrader(userId.Text, fName.Text, lName.Text);
+                await DisplayAlert("Alert", "Sucessful register with user ID" + userId.Text, "Ok");
+                await Navigation.PopAsync();
+            }
+        }
+	}
+}
