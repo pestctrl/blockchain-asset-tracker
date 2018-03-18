6d983ac69ba488ca6f104a5c4f1007e6649ec2d5
Benson Chu <bensonchu457@gmail.com>
2018-03-18 04:27:49 +0000

    made seed script more verbose

--- code/composer-blockchain/seed/seed.py
+++ code/composer-blockchain/seed/seed.py
@@ -2,6 +2,7 @@ import requests
 from string import ascii_uppercase
 
 url = 'http://129.213.108.205:3000/api/org.acme.biznet.Trader'
+url2 = 'http://129.213.108.205:3000/api/org.acme.biznet.Property'
 
 def makeTrader(i, fname, lname):
     data = { 
@@ -16,6 +17,7 @@ def makeTraders():
     with open("traders.txt","r") as file:
         for line in file:
             split = line.split(" ")
+            print("Making %s %s" % (split[1], split[2]))
             makeTrader(split[0],split[1],split[2].rstrip('\n'))
 
 def makeProperty(i, description, ownerId):
@@ -25,10 +27,11 @@ def makeProperty(i, description, ownerId):
         "description": description, 
         "owner": "TRADER%d" % ownerId
     }
-    result = requests.post(url,data=data)
+    result = requests.post(url2,data=data)
 
 def makeProperties():
     for i in ascii_uppercase:
+        print("Making property %s" % i)
         makeProperty(i,
                      "Test description",
                      ord(i)%5+1)
