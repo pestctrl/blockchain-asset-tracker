885f913548cdf0f31ba0e1533796933fb414ccb6
Benson Chu <bensonchu457@gmail.com>
2018-03-25 05:49:05 +0000

    Test coverage script added...

---
+++ code/BlockchainApp/testcoverage.cmd
@@ -0,0 +1,22 @@
+@echo off
+dotnet restore
+dotnet build
+
+cd BlockchainAPI.Tools
+
+dotnet minicover instrument --workdir ../ --assemblies BlockchainAPI.Tests/**/bin/**/*.dll --sources BlockchainAPI/**/*.cs
+dotnet minicover reset
+
+cd ..
+
+dotnet test --no-build BlockchainAPI.Tests/BlockchainAPI.Tests.csproj
+
+cd BlockchainAPI.Tools
+
+dotnet minicover uninstrument --workdir ../
+
+dotnet minicover htmlreport --workdir ../ --threshold 90
+
+dotnet minicover report --workdir ../ --threshold 90
+
+cd ..
