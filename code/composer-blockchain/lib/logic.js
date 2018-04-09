0aba3266ff202687a37a612fa224b88aac616883
Benson Chu <bensonchu457@gmail.com>
2018-04-09 14:45:59 +0000

    More reverts

--- code/composer-blockchain/lib/logic.js
+++ code/composer-blockchain/lib/logic.js
@@ -15,31 +15,31 @@
 'use strict';
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.example.biznet.Trade} trade - the trade to be processed
+ * @param {org.acme.biznet.Trade} trade - the trade to be processed
  * @transaction
  */
 function tradeAsset(trade) {
     trade.property.owner = trade.newOwner;
-    return getAssetRegistry('org.example.biznet.Property')
+    return getAssetRegistry('org.acme.biznet.Property')
         .then(function (assetRegistry) {
             return assetRegistry.update(trade.property);
         });
 }
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.example.biznet.Transfer} transfer - the trade to be processed
+ * @param {org.acme.biznet.Transfer} transfer - the trade to be processed
  * @transaction
  */
 async function transferPackage(transfer) {
 	// Update handler in package
 	let pack = transfer.package;
 	pack.handler = transfer.newHandler;
-	let packRegistry = await getAssetRegistry('org.example.biznet.Package');
+	let packRegistry = await getAssetRegistry('org.acme.biznet.Package');
 	await packRegistry.update(pack);
 	
 	// Update all properties to new owner
 	let fprop = pack.contents[0];
 	fprop.owner = transfer.newHandler;
-	let propRegistry = await getAssetRegistry('org.example.biznet.Property');
+	let propRegistry = await getAssetRegistry('org.acme.biznet.Property');
 	propRegistry.update(fprop);
 }
