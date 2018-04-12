23d98bf7c435a6c361d54485a003d0b816d7d4e6
Xing Weng <xweng49@gmail.com>
2018-04-12 23:11:53 +0000

    Added flask authentication client

---
+++ code/BlockchainApp/BlockchainAPI/FlaskAuthentication.cs
@@ -0,0 +1,24 @@
+﻿using BlockchainAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace BlockchainAPI
+{
+    public class FlaskAuthentication
+    {
+        IBlockchainService service; 
+
+        public FlaskAuthentication(IBlockchainService service)
+        {
+            this.service = service;
+        }
+
+
+    }
+}
---
+++ code/BlockchainApp/BlockchainAPI/FlaskConsts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockchainAPI
+{
+    class FlaskConsts
+    {
+        public const String LoginUrl = "129.213.108.205:5000/login";
+        public const String RegistrationUrl = "129.213.108.205:5000/registration";
+        public const String LogoutAccessTokenUrl = "129.213.108.205:5000/logout/access";
+        public const String LogoutRefreshTokenUrl = "129.213.108.205:5000/logout/refresh";
+    }
+}
