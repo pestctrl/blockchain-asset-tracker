e5d564b3836aad9666697fe14a72b6ccc874d8ce
Son Truong <sqtruong@uh.edu>
2018-04-16 06:35:11 +0000

    updated storage permissions on android for saving qr image implemented saving qr image service on android

--- code/BlockchainApp/BlockchainApp/BlockchainApp.Android/DroidGenerateQRImage.cs
+++ code/BlockchainApp/BlockchainApp/BlockchainApp.Android/DroidGenerateQRImage.cs
@@ -11,14 +11,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Graphics;
+using Android.Systems;
 
 using ZXing.Mobile;
 using ZXing.Net.Mobile.Android;
 
-[assembly: Xamarin.Forms.Dependency(typeof(BlockchainApp.Droid.BarcodeGeneratorImplementation))]
+[assembly: Xamarin.Forms.Dependency(typeof(BlockchainApp.Droid.QRServiceImplementation))]
 namespace BlockchainApp.Droid
 {
-    public class BarcodeGeneratorImplementation :BarcodeGenerator
+    public class QRServiceImplementation : QRServices
     {
         public Stream GenerateQRImage(string codeText)
         {
@@ -36,9 +37,18 @@ namespace BlockchainApp.Droid
             barcodeWriter.Renderer = new ZXing.Mobile.BitmapRenderer();
             var bitmap = barcodeWriter.Write(codeText);
             var stream = new MemoryStream();
-            bitmap.Compress(Bitmap.CompressFormat.Png, 100, stream);
+            bitmap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
             stream.Position = 0;
             return stream;
+        }
+
+        public void SaveQRImage(Stream imageStream, string fileName)
+        {
+            var dir = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads);
+            var filePath = new Java.IO.File(dir, fileName + ".jpg");
+            var stream = new MemoryStream();
+            imageStream.CopyTo(stream);
+            File.WriteAllBytes(filePath.ToString(), stream.ToArray());
         }
     }
 }
