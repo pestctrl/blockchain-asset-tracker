64ae45212c9a7eb4b9181e706a28a82180c17ce9
Giang Ha <gi4ngh495@gmail.com>
2018-04-16 03:17:13 +0000

    flash canary test

---
+++ code/flask/test.py
@@ -0,0 +1,13 @@
+import unittest
+from re import  resources
+
+
+class test(unittest.TestCase):
+	
+    def test_canary(self):
+        self.assertTrue(True)
+
+        
+
+if __name__ == '__main__':
+        unittest.main()
