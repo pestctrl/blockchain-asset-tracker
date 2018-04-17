93f33be930585aed26e486a9e235a2a201d2cec0
Giang Ha <gi4ngh495@gmail.com>
2018-04-17 05:24:34 +0000

    add some tests

--- code/flask/test.py
+++ code/flask/test.py
@@ -1,13 +1,25 @@
-import unittest
-from re import  resources
-
+import unittest, resources, models
+from flask_restful import Resource, reqparse
+from models import UserModel, RevokedTokenModel
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+from flask import jsonify, json
+from run import db
 
 class test(unittest.TestCase):
-	
-    def test_canary(self):
-        self.assertTrue(True)
 
-        
+        def setUp(self):
+            self.UserModel = models.UserModel()
 
+        def test_canary(self):
+            self.assertTrue(True)
+
+        def test_generateHash(self):
+            hash = self.UserModel.generate_hash("hash")
+            self.assertTrue(hash)
+
+        def test_verifyHash(self):
+            hash = self.UserModel.generate_hash("hash")
+            self.assertTrue(self.UserModel.verify_hash("hash",hash))
+            
 if __name__ == '__main__':
         unittest.main()
