001aa56af03e4dd829e680ae86a402e0e4ccf22f
Giang Ha <gi4ngh495@gmail.com>
2018-04-18 06:01:33 +0000

    add import

--- code/flask/test.py
+++ code/flask/test.py
@@ -1,9 +1,10 @@
-import unittest, resources, models
+import unittest
+import run, resources, models
 from flask_restful import Resource, reqparse
 from models import UserModel, RevokedTokenModel
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 from flask import jsonify, json
-from run import db
+
 
 class test(unittest.TestCase):
 
@@ -20,6 +21,10 @@ class test(unittest.TestCase):
         def test_verifyHash(self):
             hash = self.UserModel.generate_hash("hash")
             self.assertTrue(self.UserModel.verify_hash("hash",hash))
+
+        def test_a(self):
+            hash = self.UserModel.find_by_username("john")
+            self.assertTrue(hash)
             
 if __name__ == '__main__':
         unittest.main()
