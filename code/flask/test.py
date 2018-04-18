901f38e018b4f33fa45aed5471919b19998237c3
Xing Weng <xweng49@gmail.com>
2018-04-18 23:26:21 +0000

    added testing for model

--- code/flask/test.py
+++ code/flask/test.py
@@ -1,5 +1,5 @@
 import unittest
-import run, resources, models
+import run, models, resources
 from flask_restful import Resource, reqparse
 from models import UserModel, RevokedTokenModel
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
@@ -7,9 +7,8 @@ from flask import jsonify, json
 
 
 class test(unittest.TestCase):
-
         def setUp(self):
-            self.UserModel = models.UserModel()
+			self.UserModel = models.UserModel()
 
         def test_canary(self):
             self.assertTrue(True)
@@ -22,9 +21,20 @@ class test(unittest.TestCase):
             hash = self.UserModel.generate_hash("hash")
             self.assertTrue(self.UserModel.verify_hash("hash",hash))
 
-        def test_a(self):
-            hash = self.UserModel.find_by_username("john")
-            self.assertTrue(hash)
-            
+        def test_getUser(self):
+            currentUser = self.UserModel.get_user("Smith")
+            self.assertTrue(currentUser)
+
+        def test_revokeToken(self):
+            currentUser = UserModel.find_by_username('Smith')
+            jti=currentUser.password
+            revoked_token=RevokedTokenModel(jti=jti).add()
+            self.assertTrue(models.RevokedTokenModel.is_jti_blacklisted(jti=jti))
+            self.assertTrue(models.RevokedTokenModel.query.filter_by(jti=jti))
+
+
+
+
+
 if __name__ == '__main__':
         unittest.main()
