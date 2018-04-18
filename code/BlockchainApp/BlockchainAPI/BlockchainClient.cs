0d98afd8c50c15e5ed67be34247c1fa32edf2a0a
Tom Vuong <Ntvuong@uh.edu>
2018-04-18 09:54:37 +0000

    Add method GetBlockchainservice

--- code/BlockchainApp/BlockchainAPI/BlockchainClient.cs
+++ code/BlockchainApp/BlockchainAPI/BlockchainClient.cs
@@ -19,6 +19,11 @@ namespace BlockchainAPI
             blockchainService = blockChain;
         }
 
+        public IBlockchainService GetBlockChainService()
+        {
+            return blockchainService;
+        }
+
         public async Task<bool> userExists(string text)
         {
             try
