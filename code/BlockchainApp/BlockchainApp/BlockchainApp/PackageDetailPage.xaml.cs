fc30d468e526bf9298616f79f3518b6c0ab7270b
Tom Vuong <Ntvuong@uh.edu>
2018-04-26 09:24:50 +0000

    Add PackageDetailPage. When we tab on package in packagepage, it will transfer to this packageDetailPage

---
+++ code/BlockchainApp/BlockchainApp/BlockchainApp/PackageDetailPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using BlockchainAPI;
+using BlockchainAPI.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlockchainApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PackageDetailPage : ContentPage
+	{
+        BlockchainClient client;
+        CreatePackage package;
+		public PackageDetailPage (CreatePackage package, BlockchainClient client)
+		{
+            this.client = client;
+            this.package = package;
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+            BindingContext = package;
+            recipient.Text = "Recipient: " + package.recipient.Substring(35);
+            contents.Text = "Properties: " + GetProperties(package);
+		}
+
+        string GetProperties(CreatePackage package)
+        {
+            string properties = "";
+            for (int i = 0; i < package.contents.Count; i++)
+            {
+                string property =  package.contents[i].Substring(37);
+                property = property.Replace("%20", " ");
+                if (i == package.contents.Count - 1)
+                    properties = properties + property + ".";
+                else
+                    properties = properties + property + ", ";
+            }
+
+            return properties;
+        }
+
+        void Unbox(object sender, EventArgs e)
+        {
+           
+        }
+
+        async void Back(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+	}
+
+ 
+
+}
