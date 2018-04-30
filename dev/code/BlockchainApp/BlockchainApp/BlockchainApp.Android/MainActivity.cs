d22d1b2c8e7123d1ca779b326ae4e84d4d626aeb
Benson Chu <bensonchu457@gmail.com>
2018-04-30 22:12:32 +0000

    Changes for dev branch

--- dev/code/BlockchainApp/BlockchainApp/BlockchainApp.Android/MainActivity.cs
+++ dev/code/BlockchainApp/BlockchainApp/BlockchainApp.Android/MainActivity.cs
@@ -23,12 +23,20 @@ namespace BlockchainApp.Droid
             UserDialogs.Init(this);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
-            LoadApplication(new App());
+            try
+            {
+                LoadApplication(new App());
+            }
+            catch (Exception e)
+            {
+                
+            }
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
             ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
