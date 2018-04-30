065ea34cb786eaf33f09a3b4f2134cc24da96f2b
Paul Pham <pham.paul.q@gmail.com>
2018-04-30 06:01:22 +0000

    Added HistoryPage to TabbedPage1

--- code/BlockchainApp/BlockchainApp/BlockchainApp/TabbedPage1.xaml.cs
+++ code/BlockchainApp/BlockchainApp/BlockchainApp/TabbedPage1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BlockchainAPI;
 using BlockchainAPI.Models;
+using BlockchainAPI.Transactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,16 +27,27 @@ namespace BlockchainApp
             InitializeComponent();
             this.Title = String.Format("Hello, {0}!", client.thisTrader.fullName);
             NavigationPage.SetHasNavigationBar(this, false);
+          
 
             var userPage = new NavigationPage(new UserPage(client));
             var packagesPage = new NavigationPage(new PackagesPage(client));
+   
 
             userPage.Title = "My Properties";
             packagesPage.Title = "My Packages";
+            historyPage.Title = "My History";
 
             InitializeComponent();
             Children.Add(userPage);
             Children.Add(packagesPage);
+        }
+
+        async protected override void OnAppearing()
+        {
+            List<CreatePackage> history = new List<CreatePackage>(await client.GetUserTransactions());
+            var historyPage = new NavigationPage(new HistoryPage(client, history));
+            historyPage.Title = "My History";
+            Children.Add(historyPage);
         }
     }
 }
