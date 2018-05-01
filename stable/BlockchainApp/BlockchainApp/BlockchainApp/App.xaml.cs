f70a8942f7e40de16424a472c1095c2c563745a7
Benson Chu <bensonchu457@gmail.com>
2018-05-01 22:05:18 +0000

    Added stable branch

---
+++ stable/BlockchainApp/BlockchainApp/BlockchainApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using BlockchainAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BlockchainApp
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+            InitializeComponent();
+            MainPage = new NavigationPage(new BlockchainApp.MainPage(new HyperledgerService())) {
+                BarBackgroundColor = Color.FromRgb(5, 5, 5)
+            };
+		}
+
+		protected override void OnStart () {}
+
+		protected override void OnSleep () {}
+
+		protected override void OnResume () {}
+	}
+}
---
+++ stable/BlockchainApp/BlockchainWebApp/Controllers/TransferController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlockchainAPI;
+using BlockchainAPI.Models;
+using BlockchainAPI.Transactions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockchainWebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Transfer")]
+    public class TransferController : Controller
+    {
+        BlockchainClient client;
+
+        public TransferController(IBlockchainService service)
+        {
+            client = new BlockchainClient(service);
+        }
+
+        // GET: api/Transaction
+        [HttpGet]
+        public async Task<IEnumerable<NewTransfer>> Get()
+        {
+            return await client.GetAllTransfers();
+        }
+
+        // GET: api/Transaction/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet("History/{id}")]
+        public async Task<IEnumerable<NewTransfer>> GetPropertyHistory(string id)
+        {
+            return await client.GetPropertyHistory(id);
+        }
+    }
+}
