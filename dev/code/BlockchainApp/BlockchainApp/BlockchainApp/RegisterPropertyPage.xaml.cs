c175b1b1d196363f3ea9251d60ef09d80c4da4c0
Benson Chu <bensonchu457@gmail.com>
2018-05-01 19:19:48 +0000

    Added page changes for camera

--- dev/code/BlockchainApp/BlockchainApp/BlockchainApp/RegisterPropertyPage.xaml.cs
+++ dev/code/BlockchainApp/BlockchainApp/BlockchainApp/RegisterPropertyPage.xaml.cs
@@ -46,5 +46,13 @@ namespace BlockchainApp
 
             await Navigation.PopAsync();
         }
-	}
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
 }
