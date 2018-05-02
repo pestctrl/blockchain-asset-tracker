3e9d0a6d9e6e25ceef8a6618ab38d3a6646d979b
Son Truong <sqtruong@uh.edu>
2018-05-02 21:06:09 +0000

    image view

---
+++ code/BlockchainApp/BlockchainApp/BlockchainApp/PropertyViewPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlockchainAPI;
+using BlockchainAPI.Models;
+using BlockchainAPI.Transactions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BlockchainApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PropertyViewPage : ContentView
+	{
+        BlockchainClient client;
+        Property property;
+
+        public PropertyViewPage ()
+		{
+			InitializeComponent ();
+		}
+
+        public PropertyViewPage(BlockchainClient client, Property property)
+        {
+            this.client = client;
+            this.property = property;
+
+            InitializeComponent();
+            propertyImage.Source = ImageSource.FromStream(()=> property.image);
+        }
+
+        async void Back(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
