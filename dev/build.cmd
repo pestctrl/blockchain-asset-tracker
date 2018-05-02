179996ac36c5e54c9ce22b7658bea7612b994fd5
Benson Chu <bensonchu457@gmail.com>
2018-05-02 00:29:23 +0000

    Removed old dev branch

--- dev/build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd code\BlockchainApp\BlockchainAPI.Tests
-dotnet build
-dotnet test --logger trx
-cd ..
-call testcoverage.cmd
-
-exit %ERRORLEVEL%
--- dev/code/composer-blockchain/helper-scripts/ping.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-composer card import --file networkadmin.carde
-composer network ping --card admin@tutorial-network
